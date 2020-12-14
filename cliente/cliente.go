13f4fddf125083d5a35c0b1771a06987b0f4269b
Raul Alvarez <raul.alvarezc@sansano.usm.cl>
2020-12-14 19:17:33 -0300

    añadido separador de comandos con estandarizacion a minusculas

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -4,12 +4,21 @@ import (
 	"context"
 	"fmt"
 	"time"
+	"strings"
 	pb "github.com/sirbernal/lab3SD/proto/client_service"
 	"google.golang.org/grpc"
 
 )
 
 var timeout = time.Duration(1)*time.Second
+
+func DetectCommand(comm string)[]string{
+	str:= strings.Split(comm, " ")
+	var resp []string
+	resp=append(resp,strings.ToLower(str[0]))
+	resp=append(resp,strings.Join(str[1:]," "))
+	return resp
+}
 
 func SolicitarIP(){
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure()) //genera la conexion con el broker
@@ -34,8 +43,7 @@ func SolicitarIP(){
 
 
 func main() {
-	
+	fmt.Println(DetectCommand("Hola, que sucede mi pana"))
 	fmt.Println("Hola")
-	SolicitarIP()
-
+	//SolicitarIP()
 }
